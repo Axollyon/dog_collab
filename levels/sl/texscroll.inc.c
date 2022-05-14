0f04a2e93fab49035ea221620b895adea353ab10
Axollyon <20480418+Axollyon@users.noreply.github.com>
2022-05-14 02:26:20 -0400

    foggify the village

--- levels/sl/texscroll.inc.c
+++ levels/sl/texscroll.inc.c
@@ -28,35 +28,35 @@ void scroll_sts_mat_sl_dl_Logs_layer1_area1() {
 	}
 };
 
-void scroll_sts_mat_sl_dl_Metal_White_layer1() {
+void scroll_sts_mat_sl_dl_Metal_White_layer1_area1() {
 	static int intervalTex0 = 2;
 	static int curInterval0 = 2;
-	Gfx *mat = segmented_to_virtual(mat_sl_dl_Metal_White_layer1);
+	Gfx *mat = segmented_to_virtual(mat_sl_dl_Metal_White_layer1_area1);
 
 	if (--curInterval0 <= 0) {
-		shift_t(mat, 13, PACK_TILESIZE(0, 1));
+		shift_t(mat, 15, PACK_TILESIZE(0, 1));
 		curInterval0 = intervalTex0;
 	}
 };
 
-void scroll_sts_mat_sl_dl_Planks_layer1() {
+void scroll_sts_mat_sl_dl_Planks_layer1_area1() {
 	static int intervalTex0 = 2;
 	static int curInterval0 = 2;
-	Gfx *mat = segmented_to_virtual(mat_sl_dl_Planks_layer1);
+	Gfx *mat = segmented_to_virtual(mat_sl_dl_Planks_layer1_area1);
 
 	if (--curInterval0 <= 0) {
-		shift_s_down(mat, 13, PACK_TILESIZE(0, 1));
+		shift_s_down(mat, 15, PACK_TILESIZE(0, 1));
 		curInterval0 = intervalTex0;
 	}
 };
 
-void scroll_sts_mat_sl_dl_Metal_Black_layer1() {
+void scroll_sts_mat_sl_dl_Metal_Black_layer1_area1() {
 	static int intervalTex0 = 2;
 	static int curInterval0 = 2;
-	Gfx *mat = segmented_to_virtual(mat_sl_dl_Metal_Black_layer1);
+	Gfx *mat = segmented_to_virtual(mat_sl_dl_Metal_Black_layer1_area1);
 
 	if (--curInterval0 <= 0) {
-		shift_t(mat, 13, PACK_TILESIZE(0, 1));
+		shift_t(mat, 15, PACK_TILESIZE(0, 1));
 		curInterval0 = intervalTex0;
 	}
 };
@@ -86,9 +86,9 @@ void scroll_sts_mat_sl_dl_Logs_Fade_layer7() {
 void scroll_sl() {
 	scroll_sts_mat_sl_dl_Water_layer5();
 	scroll_sts_mat_sl_dl_Logs_layer1_area1();
-	scroll_sts_mat_sl_dl_Metal_White_layer1();
-	scroll_sts_mat_sl_dl_Planks_layer1();
-	scroll_sts_mat_sl_dl_Metal_Black_layer1();
+	scroll_sts_mat_sl_dl_Metal_White_layer1_area1();
+	scroll_sts_mat_sl_dl_Planks_layer1_area1();
+	scroll_sts_mat_sl_dl_Metal_Black_layer1_area1();
 	scroll_sts_mat_sl_dl_Planks_Fade_layer7();
 	scroll_sts_mat_sl_dl_Logs_Fade_layer7();
 }
